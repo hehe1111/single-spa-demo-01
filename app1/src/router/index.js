fad179ca1fddc0d39d864529653326633789e3fe
hehe1111 <2908749709@qq.com>
2021-07-10 19:03:33 +0800

    修改路由文件

--- app1/src/router/index.js
+++ app1/src/router/index.js
@@ -22,7 +22,8 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  // 通过环境变量来配置路由的 base url
+  base: process.env.VUE_APP_BASE_URL,
   routes
 })
 
